cbf61bf0ad6ab349497ee5c4bc088edff1fddeec
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-05-29 21:48:53 -0300

    GoReactNative - Configurando Header

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import Header from '~/components/Header';
 
 // import { Container } from './styles';
 
 const Repositories = () => (
   <View>
-    <Text>Repositories</Text>
+    <Header title="Repositórios" />
   </View>
 );
 
